694df719153dcf2e89b445dfa7447e1abbedaf7a
Chefmoderator <148126189+Chefmoderator@users.noreply.github.com>
2025-01-19 10:40:40 +0200

    planer.py

--- planer.py
+++ planer.py
@@ -1,19 +1,61 @@
-import sqlite3,time
+import sqlite3
+from datetime import datetime, timedelta
+from itertools import count
 
-project = {}
-obj = time.gmtime()
+
+import telebot
+
+project_bd = {}
+project_time = None
+project_name = None
 db = sqlite3.connect('index.db')
 sql = db.cursor()
 sql.execute('''CREATE TABLE IF NOT EXISTS projects (
-project_day TEXT
+    project TEXT
 )''')
-db.commit()
-user_plan = input("Your plane >")
-user_data = input("Write when you want work with your project >")
-project[(user_data)] = user_plan
-print(project)
-sql.execute('''SELECT project_day FROM projects''')
-if sql.fetchone() is None:
-    sql.execute('INSERT INTO projects FROM (?)', (project[(user_data)]))
 
-db.close()
+class Planner:
+    def proj(self, message):
+        from bot import bot
+        global project_time
+
+        markup = telebot.types.ReplyKeyboardMarkup(resize_keyboard=True)
+        for i in range(30):
+            time = datetime.now() + timedelta(days=i)
+            item = telebot.types.KeyboardButton(time.strftime("%Y-%m-%d"))
+            markup.add(item)
+
+        bot.send_message(message.chat.id, "Выберите дату", reply_markup=markup)
+
+
+    def handle_date_selection(self, message):
+        global project_time
+        from bot import bot
+        print(f"Message text: {message.text}")
+        if message.text != 'planers':
+            project_time = message.text
+            print(f"Selected project_time: {project_time}")
+            bot.send_message(message.chat.id, f"Дата выбрана: {project_time}")
+
+
+    def project_name(self,message):
+        from bot import bot
+        bot.send_message(message.chat.id, "Напишите названия проекта ")
+
+
+
+
+            # sql.execute('''INSERT INTO projects (project) VALUES (?)''', (project_bd[num],))
+
+    def close_db():
+        db.commit()
+        db.close()
+
+
+
+
+
+
+
+
+
